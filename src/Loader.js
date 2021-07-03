99f5a2d72a697fab0bc856e7bf394c76960d4d26
Herman Murauyou <geraacorn@mail.ru>
2021-07-03 22:50:55 +0200

    ReactJS

---
+++ src/Loader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+<div style={{display: 'flex', justifyContent: 'center', margin: '.rem' }}>
+    <div className="lds-ripple"><div></div><div></div></div>
+</div> 
+)
